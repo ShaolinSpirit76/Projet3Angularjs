cdd62dda6f9562b5160a31677a65f9ce16d62156
ShaolinSpirit76 <paul.compere76@gmail.com>
2019-05-13 12:47:40 +0200

    panier fonctionnel

--- demon.js
+++ demon.js
@@ -1,8 +1,9 @@
 var app = angular.module('demonApp',['ngRoute']);
 var totalPrice = 0;
-var totalAmount = 0;
 var index = 0;
-var finish = 0;
+var productQuantity = 0;
+var totalAmount = 0;
+
 app.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider
@@ -60,87 +61,79 @@ app.run(['$rootScope','$http', function($rootScope,$http) {
 
 
 app.controller('articlesCtrl',['$scope','$rootScope', function($scope, $rootScope){
-   $scope.addToCart = function(article){
-   if(article.quantity == 0){
-      article.quantity++;
-      article.totalPrice = article.quantity*parseFloat(article.price);
-      totalAmount += parseFloat(article.price);
-        console.log(totalAmount);
-      article.position = index;
-      index++;
-      $rootScope.cart.push({
-        name : article.name,
-        ref : article.ref,
-        price : article.price,
-        quantity : article.quantity,
-        img : article.img,
-        clicked : article.clicked,
-        totalPrice:article.totalPrice,
-        totalAmount:article.totalAmount,
-        position:article.position
-      });
-   }else{
-     console.log(article.position);
-     $rootScope.cart[article.position].quantity++;
-     $rootScope.cart[article.position].totalPrice = $rootScope.cart[article.position].quantity*parseFloat(article.price);
-     totalAmount += parseFloat(article.price);
-       console.log(totalAmount);
-   }
-   }
+  $scope.addToCart = function(articleToCart) {
+    var found = $rootScope.cart.find(x => x.id === articleToCart.id);
 
-  $scope.oneMore = function(product){
-  console.log(product);
-   $rootScope.cart[product.position].quantity++;
-   product.totalPrice = product.quantity*parseFloat(product.price);
-   totalAmount += parseFloat(product.price);
-   console.log(totalAmount);
- }
+    if (found) {
+      var indexArticle = $rootScope.cart.indexOf(found);
+      $rootScope.cart[indexArticle].quantity++;
+      $rootScope.cart[indexArticle].totalPrice = Math.round($rootScope.cart[indexArticle].quantity * $rootScope.cart[indexArticle].price * 100) / 100
+      productQuantity++;
+    } else {
 
- $scope.oneLess = function(product){
-   console.log(product);
-    $rootScope.cart[product.position].quantity--;
-    product.totalPrice = product.quantity*parseFloat(product.price);
-    totalAmount -= parseFloat(product.price);
-    console.log(totalAmount);
-    if($rootScope.cart[product.position].quantity==0){
-      $rootScope.cart.splice(product.position,1);
-      console.log(index);
-      index--;
-      console.log(index);
+      $rootScope.cart.push(angular.copy(articleToCart));
+      $rootScope.cart[index].totalPrice = Math.round(articleToCart.price * 100) / 100;
+      index++ ;
+      productQuantity++;
     }
-  }
+  };
 
-  $scope.remove = function(product){
-    product.quantity = 0;
-    if($rootScope.cart[product.position].quantity==0){
-      $rootScope.cart.splice(product.position,1);
-      console.log(index);
-      index--;
-      console.log(index);
-        console.log(totalAmount);
-    }
+
+  $scope.removeArticle = function(removeArticle) {
+    var indexArticle = $rootScope.cart.indexOf(removeArticle);
+    productQuantity -= $rootScope.cart[indexArticle].quantity;
+    $rootScope.cart.splice(indexArticle, 1);
+    index--;
+  };
     // $rootScope.cart.splice(product.position,1);
     // index--;
-  }
 
-  $scope.total = function(product) {
-    var totalAmount = 0;
-      // totalAmount += parseFloat(product.totalPrice);
-    if (totalAmount == undefined) {
-      totalAmount = 0.00;
-    }
-      console.log(totalAmount);
-// $(".pay").data("total", totalAmount).children(".total").html(totalAmount);
-  }
+    $scope.totalAmount = function() {
+      var total = 0;
+      angular.forEach($rootScope.cart, function(value, key) {
+        total = Math.round((total + value.quantity * value.price) * 100) / 100;
+      });
+      totalAmount = total;
+      return total;
+    };
 
   $scope.stop = function() { // Fonction au click du bouton valider ma commande pour stopper la boucle
-    finish++
     Swal.fire("Votre commande est de " + totalAmount + "€");
   }
 
-  // $scope.keepOn = function() {
-  //   $scope.tag="false";
-  // }
+  $scope.oneLess = function(product) {
+    product.totalPrice = Math.round(product.quantity * product.price * 100) / 100;
+    if (productQuantity >= 1){
+    productQuantity--;
+  }
+  }
+
+  $scope.oneMore = function(product) {
+    product.totalPrice = Math.round(product.quantity * product.price * 100) / 100;
+    productQuantity++;
+  }
+
+  $scope.productTotalQuantity = function(){
+    if(productQuantity > 0){
+    return productQuantity;
+  }
+}
+
+// $scope.tag="false";
+
+//   $scope.keepOn = function() {
+//     // $scope.tag="true";
+//
+// it('should check ngHide', function() {
+//   var checkbox = element(by.model('clicked'));
+//
+//   expect(checkElem.isDisplayed()).toBe(false);
+//   button.click();
+//   expect(checkElem.isDisplayed()).toBe(f);
+// });
+
+
+
 }]);
 
 // } Fermeture de la boucle
